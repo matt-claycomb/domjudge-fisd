c12c997ca3746031811993f4d5c71668e4e3f2b6
Matt Hermes <msh160130@gmail.com>
2017-10-27 12:56:01 -0500

    Update index.php

--- www/api/index.php
+++ www/api/index.php
@@ -383,7 +383,7 @@ function judgings_POST($args)
 	               time_factor*timelimit AS maxruntime,
 	               p.memlimit, p.outputlimit,
 	               special_run AS run, special_compare AS compare,
-	               special_compare_args AS compare_args, compile_script
+	               special_compare_args AS compare_args, compile_script, filename
 	               FROM submission s
 	               LEFT JOIN problem p USING (probid)
 	               LEFT JOIN language l USING (langid)
