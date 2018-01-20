33f9a4c44d6d83b604ce4c99ec4104e9f2204c49
Matt Hermes <msh160130@gmail.com>
2018-01-20 14:56:03 -0600

    Fix Testcase Download Filename
    Fix so when downloading testcase for problem with file input, the downloaded file has the name expected at submission. No behavior change for stdin problems.

--- lib/www/common.php
+++ lib/www/common.php
@@ -572,7 +572,7 @@ function putSampleTestcase($probid, $seq, $type)
 {
 	global $DB, $cdata;
 
-	$sample = $DB->q('MAYBETUPLE SELECT shortname, ' . $type . 'put AS content
+	$sample = $DB->q('MAYBETUPLE SELECT shortname, ' . $type . 'put AS content, filename
 	                  FROM problem INNER JOIN testcase USING (probid)
 	                  INNER JOIN contestproblem USING (probid)
 	                  WHERE probid = %i AND cid = %i AND allow_submit = 1
@@ -584,7 +584,7 @@ function putSampleTestcase($probid, $seq, $type)
 	}
 	$probname = $sample['shortname'];
 
-	$filename = "sample-$probname.$seq.$type";
+	$filename = $sample['filename']??"sample-$probname.$seq.$type";
 
 	header("Content-Type: text/plain; name=\"$filename\"");
 	header("Content-Disposition: attachment; filename=\"$filename\"");
